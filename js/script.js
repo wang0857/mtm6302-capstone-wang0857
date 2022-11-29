ce3ed0a745a6e840fab349f32c27ee19d1e39d05
Ruo-Fang Wang <wang0857@algonquinlive.com>
2022-11-29 17:19:20 -0500

    created the js - collapse the sidebar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
-// TODO: Sidebar collapse
+// TODO: Collapse the sidebar
+let sidebar = document.querySelector("aside");
+let collapseBtn = document.querySelector(".collapse-btn");
+let btnArrow = document.querySelector(".collapse-btn i");
+let sidebarItems = document.querySelectorAll(".side-nav li a span");
+let appHeader = document.querySelector(".app-header");
+let appBtns = document.querySelectorAll(".download-btn");
+let appItems = document.querySelectorAll(".download-btn span");
+
+
+collapseBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("collapsed-sidebar")
+    sidebarItems.forEach((item) => {
+        item.classList.toggle("collapse");
+    });
+
+    appBtns.forEach((btn) => {
+        btn.classList.toggle("width-auto");
+    });
+    appHeader.classList.toggle("collapse");
+    appItems.forEach((item) => {
+        item.classList.toggle("collapse");
+    });
+
+    btnArrow.classList.toggle("flip");
+});
+
 // TODO: Fetch APOD API
 // TODO: Search Function
 // TODO: Submission (Need to decide whether to build a DB for it)
